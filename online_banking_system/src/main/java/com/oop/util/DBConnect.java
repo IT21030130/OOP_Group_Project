7df68d8f706474a997494124299df2be86bde105
J.K.D.S.Samara <87611343+IT21030130@users.noreply.github.com>
2022-07-26 22:21:59 +0530

    Add files via upload

--- online_banking_system/src/main/java/com/oop/util/DBConnect.java
+++ online_banking_system/src/main/java/com/oop/util/DBConnect.java
@@ -4,26 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnect {
-	//declaring private static variables 
 	private static String url = "jdbc:mysql://localhost:3306/online_banking_system";
 	private static String userName = "root";
-	private static String password = "sunera";
+	private static String password = "sakuni";
 	private static Connection con;
 	
 	public static Connection getConnection() {
-		
-		// Exception Handling using try-catch
 		try {
-			
 			Class.forName("com.mysql.jdbc.Driver");
 			
-			con = DriverManager.getConnection(url,userName,password);
-			
-		}catch(Exception e) {
-			System.out.println("DB connectionm not success");
+			con = DriverManager.getConnection(url, userName, password);
 		}
-		
+		catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Database connection is not success!!!");
+		}
 		return con;
-		
-}
+	}
 }
